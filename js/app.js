f8c3e5bc0e6b4af3d130472e92aefdf1a93ea326
StefanCar156 <stefancar156@gmail.com>
2021-02-26 12:18:28 +0100

    Fixed Responsiveness

--- js/app.js
+++ js/app.js
@@ -1,57 +1,26 @@
-// Testimonials Section
-
-const nextBtn = document.querySelector("#next-btn");
-const prevBtn = document.querySelector("#prev-btn");
-const testimonialsContainer = document.querySelector(".testimonials-container");
-const testimonialComponent = document.querySelector(".testimonials-component");
-const childrenNum = testimonialsContainer.childElementCount;
-let testimonialCounter = 0;
-
-nextBtn.addEventListener("click", () => {
-  if (testimonialCounter > childrenNum - 2) {
-    return;
-  }
-  testimonialCounter++;
-  testimonialsContainer.style.transform =
-    "translateX(" +
-    -testimonialCounter * testimonialComponent.clientWidth +
-    "px)";
-  testimonialsContainer.style.transition = "transform 0.4s ease";
-});
-
-prevBtn.addEventListener("click", () => {
-  if (testimonialCounter < 1) {
-    return;
-  }
-  testimonialCounter--;
-  testimonialsContainer.style.transform =
-    "translateX(" +
-    -testimonialCounter * testimonialComponent.clientWidth +
-    "px)";
-  testimonialsContainer.style.transition = "transform 0.4s ease";
-});
-
 // Responsive Styles
 function responsiveStyle() {
   const homeSection = document.querySelector("#home-section");
   const homeImg = document.querySelector(".home-image");
   const homeText = document.querySelector(".home-text");
 
-  if (window.outerWidth <= 1000) {
+  if (window.innerWidth <= 1024) {
     homeSection.insertBefore(homeImg, homeText);
-    prevBtn.src = "../img/prev-btn-2.png";
-    nextBtn.src = "../img/next-btn-2.png";
+    prevBtn.querySelector("img").src = "../img/prev-btn-2.png";
+    nextBtn.querySelector("img").src = "../img/next-btn-2.png";
   } else {
     homeSection.insertBefore(homeText, homeImg);
-    prevBtn.src = "../img/prev-btn.png";
-    nextBtn.src = "../img/next-btn.png";
+    prevBtn.querySelector("img").src = "../img/prev-btn.png";
+    nextBtn.querySelector("img").src = "../img/next-btn.png";
   }
+
+  handleSlideImages()
 }
 
 window.onresize = responsiveStyle;
 
 // Navbar Toggle
-const navBtn = document.querySelector(".menu-icon");
+const navBtn = document.querySelector(".menu-btn");
 const navbar = document.querySelector(".nav-links");
 const navLink = document.querySelectorAll(".nav-link");
 
@@ -63,6 +32,34 @@ navLink.forEach((link) => {
   link.addEventListener("click", () => {
     navbar.classList.remove("nav-links-active");
   });
+});
+
+// Fixed Nav
+let scrollPosition = 0
+const header = document.querySelector("header")
+
+function fixedNav() {
+  // Display navbar if scrolling upwards, Hide it if scrolling downwards
+  if ((document.body.getBoundingClientRect()).top > scrollPosition) {
+    header.classList.add('fixed-nav')
+  } else {
+    header.classList.remove('fixed-nav')
+  }
+  // save the new position for iteration.
+  scrollPosition = (document.body.getBoundingClientRect()).top;
+
+  // Show/hide box shadow
+  if ((document.body.getBoundingClientRect()).top > 1) {
+      header.style.boxShadow = "none"
+  }
+  else {
+      header.style.boxShadow = "2px 2px 10px 2px rgba(0, 0, 0, 0.1)"
+  }
+}
+
+window.addEventListener("scroll", () => {
+  showBackToTopBtn();
+  fixedNav();
 });
 
 // Back To Top
@@ -80,16 +77,32 @@ backToTopBtn.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
 
-// Fixed Nav
-function fixedNav() {
-  const header = document.querySelector("header");
+// Testimonials Section
 
-  window.scrollY > 0
-    ? header.classList.add("fixed-nav")
-    : header.classList.remove("fixed-nav");
+const nextBtn = document.querySelector("#next-btn");
+const prevBtn = document.querySelector("#prev-btn");
+const testimonialsContainer = document.querySelector(".testimonials-container");
+const testimonialComponent = document.querySelector(".testimonials-component");
+const childrenNum = testimonialsContainer.childElementCount;
+let testimonialCounter = 0;
+
+const handleSlideImages = () => {
+  testimonialsContainer.style.transform = "translateX(" + -testimonialComponent.clientWidth * testimonialCounter + "px)"
+  testimonialsContainer.style.transition = "transform 0.5s ease"
 }
 
-window.addEventListener("scroll", () => {
-  showBackToTopBtn();
-  fixedNav();
+nextBtn.addEventListener("click", () => {
+  if (testimonialCounter > childrenNum - 2) {
+    return;
+  }
+  testimonialCounter++;
+  handleSlideImages()
+});
+
+prevBtn.addEventListener("click", () => {
+  if (testimonialCounter < 1) {
+    return;
+  }
+  testimonialCounter--;
+  handleSlideImages()
 });
